217e5a35a1e25c6e1fa7b6dec939c245c165adfd
chandangowda <chandangowdacb@gmail.com>
2021-02-05 08:32:32 +0530

    Add files via upload

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './index.css';
+
+import img from './Sample-png-image-100kb.png';
+// Delete me
+export const Thing = () => {
+
+  return <div>
+     {/* <div className="background-image" >
+        <h1>testing</h1>
+       </div>  */}
+       <img src={img} />
+    the snozzberries taste like snozzberries</div>;
+};
